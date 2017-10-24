c8f637f6b6740b95aa1b0951c7fe1c917d1a175a
Pavel <p.a.kadantsev@gmail.com>
2017-10-24 22:55:08 +0300

    Initial commit

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,39 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import datetime
+import pytz
 
-def get_midnighters():
-    pass
+def load_database_records():
+    all_db_records = []
+    url = 'http://devman.org/api/challenges/solution_attempts/'
+    pages_count = int(url.json().get('number_of_pages'))
+    for page_number in range(1, pages_count + 1):
+        payload = {'page':page_number}
+        try:
+            records_responce = requests.get(url, params = payload).json().get('records')
+        except requests.exceptions.RequestException as error:
+            records_responce = None
+            print(error)
+        if records_responce is not None:
+            for user in records_responce:
+                all_db_records.append(user)
+    return all_db_records
+
+
+    
+    
+def get_users_info():
+    for single_record_content in load_database_records():
+        yield {'username': single_record_content['timestamp'] ,
+               'timestamp': single_record_content['username'] ,
+               'timezone': single_record_content['timezone']}
+
+
+               
+def get_midnighters(database_records):
+    devman_owls = []
+    for record in database_records:
+        devman_server_timestamp = record.get('timestamp')
+
 
 if __name__ == '__main__':
-  pass
+    pass
