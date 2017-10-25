c3fc4c26944cc93e2efff66e475a0ae86787cc66
Pavel <p.a.kadantsev@gmail.com>
2017-10-25 22:49:14 +0300

    Updated def get_midnighter and printing out

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,6 +1,6 @@
 import requests
-import datetime
-import pytz
+from datetime import datetime
+from pytz import timezone
 
 def load_database_records():
     all_db_records = []
@@ -17,23 +17,24 @@ def load_database_records():
             for user in records_responce:
                 all_db_records.append(user)
     return all_db_records
-
-
-    
-    
-def get_users_info():
-    for single_record_content in load_database_records():
-        yield {'username': single_record_content['timestamp'] ,
-               'timestamp': single_record_content['username'] ,
-               'timezone': single_record_content['timezone']}
-
-
-               
+   
 def get_midnighters(database_records):
     devman_owls = []
+    night_start, night_stop = 0,6
     for record in database_records:
         devman_server_timestamp = record.get('timestamp')
-
+        local_tz = timezone(record.get('timezone'))
+        local_time = datetime.fromtimestamp(devman_server_timestamp, tz = local_tz)
+        if local_time.hour in range (night_start, night_stop + 1):
+            devman_owls.append(record.get('username'))
+    return devman_owls
 
 if __name__ == '__main__':
-    pass
+    print('\nRetrieving midnight users from Devman.org database. Please wait.\n') 
+    if devman_owls = get_midnighter(load_database_records()):
+        print('''Done! Here is a list devman owls - users who've sent tasks for review 
+ in time from the midnight to 6.00 a.m:''')
+        for user_count, user in enumerate(devman_owls, start = 1):
+            print('{}. {}'.format(user_count, user))
+    else:
+         print('Looks like Devman.org students prefer sleep at night!')
